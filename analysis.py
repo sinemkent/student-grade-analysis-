3bd9d0d5f50c3800238ca2609162bb3ab157cf93
sinem <sinemkent0@gmail.com>
2025-12-25 21:11:40 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,1 +1,21 @@
+import pandas as pd
 
+# CSV dosyasını oku
+df = pd.read_csv("data.csv")
+
+# Veriyi göster
+print("Student Grades:\n")
+print(df)
+
+# Ders ortalamaları
+print("\nCourse Averages:")
+print("Math:", df["math"].mean())
+print("Physics:", df["physics"].mean())
+print("Computer Science:", df["cs"].mean())
+
+# En başarılı öğrenci
+df["average"] = df[["math", "physics", "cs"]].mean(axis=1)
+top_student = df.loc[df["average"].idxmax()]
+
+print("\nTop Student:")
+print(top_student)
